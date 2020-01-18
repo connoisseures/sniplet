b13a8b8e7563f911e766072192915efbff8d69c0
connoi <connoisseures@gmail.com>
2020-01-17 21:24:58 -0500

    amz oa 2020

--- amz/amz-OA-ZombieinMatrix.cpp
+++ amz/amz-OA-ZombieinMatrix.cpp
@@ -3,6 +3,20 @@
 #include <queue>          // std::queue
 using namespace std;
 
+/*
+we assume 
+1. empty grid, output 0
+2. empty zombie, output -1
+
+Thought:
+We use BFS to traverse the grid and start from the positions with its value as 1. We modify the value of the position we are visiting as 1 until we visit all of the grids. 
+
+Time complexity:
+T = O(m*n), where m is row size of grid, n is the column size of grid
+
+Space complexity:
+O(m*n)
+*/
 
 bool isValid(int i, int j, vector<vector<int>> &grid) {
     if (i < 0 || i == grid.size() || j < 0 || j == grid[0].size()) return false;
@@ -10,6 +24,7 @@ bool isValid(int i, int j, vector<vector<int>> &grid) {
 }
 
 int timeOfInfecting(vector<vector<int>> grid) {
+    if (grid.size() == 0 || grid[0].size() == 0) return 0; 
     int people = 0;
     int m = grid.size(), n = grid[0].size();
     queue<vector<int>> que;
@@ -21,8 +36,8 @@ int timeOfInfecting(vector<vector<int>> grid) {
         }
         //cout << endl;
     }
-    //if (people == 0) return 0;
-    if (people == 0) return -1;
+    if (people == 0) return 0;
+    //if (people == 0) return -1;
 
     int hours = 0;
     vector<vector<int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
@@ -53,8 +68,10 @@ int main() {
         {0, 0, 0, 0, 1},
         {0, 1, 0, 0, 0}};
     int hours = timeOfInfecting(grid);
+    cout << "case0" << endl;
     cout << "hours : " << hours << endl;
 
+    cout << "case1: no people" << endl;
     vector<vector<int>> grid1 = {
         {1, 1, 1, 1, 1},
         {1, 1, 1, 1, 1},
@@ -63,6 +80,7 @@ int main() {
     int hours1 = timeOfInfecting(grid1);
     cout << "hours : " << hours1 << endl;
 
+    cout << "case2" << endl;
     vector<vector<int>> grid2 = {
         {0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0},
@@ -71,6 +89,7 @@ int main() {
     int hours2 = timeOfInfecting(grid2);
     cout << "hours : " << hours2 << endl;
 
+    cout << "case3" << endl;
     vector<vector<int>> grid3 = {
         {0, 0, 0, 0, 0},
         {0, 0, 1, 0, 0},
@@ -78,5 +97,44 @@ int main() {
         {0, 0, 0, 0, 0}};
     int hours3 = timeOfInfecting(grid3);
     cout << "hours : " << hours3 << endl;
+
+    cout << "case4: no zombie" << endl;
+    vector<vector<int>> grid4 = {
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}};
+    int hours4 = timeOfInfecting(grid4);
+    cout << "hours : " << hours4 << endl;
+
+    vector<vector<int>> grid5 = {
+        {1}};
+    int hours5 = timeOfInfecting(grid5);
+    cout << "case5" << endl;
+    cout << "hours : " << hours5 << endl;
+
+    vector<vector<int>> grid6 = {
+        {0}};
+    int hours6 = timeOfInfecting(grid6);
+    cout << "case6" << endl;
+    cout << "hours : " << hours6 << endl;
+
+    vector<vector<int>> grid7 = {
+        {0,0}};
+    int hours7 = timeOfInfecting(grid7);
+    cout << "case7: 1d grid" << endl;
+    cout << "hours : " << hours7 << endl;
+
+    vector<vector<int>> grid8 = {
+        {0,1}};
+    int hours8 = timeOfInfecting(grid8);
+    cout << "case8: 1d grid" << endl;
+    cout << "hours : " << hours8 << endl;
+
+    vector<vector<int>> grid9 = {
+        {}};
+    int hours9 = timeOfInfecting(grid9);
+    cout << "case9: empty grid" << endl;
+    cout << "hours : " << hours9 << endl;
 }
 
